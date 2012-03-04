84853fcae413de76f5e69ffa9861e13a98837130
James Shipton <ionysis@gmail.com>
2012-03-04 21:10:55 +0000

    Return array of player_data

--- lib/football-manager/i_o_helper.rb
+++ lib/football-manager/i_o_helper.rb
@@ -1,7 +1,7 @@
 module FootballManager
   module IOHelper
     def self.fetch_player_data(player_data_reference)
-      File.read(player_data_reference)
+      File.readlines(player_data_reference)
     end
   end
 end
