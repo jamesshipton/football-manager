683e5355d5c657bddbaf9a37493bc0d1f9d2739a
James Shipton <ionysis@gmail.com>
2012-03-07 07:31:03 +0000

    Add Player#skill

--- lib/football-manager/player.rb
+++ lib/football-manager/player.rb
@@ -1,11 +1,15 @@
 module FootballManager
   class Player
 
-    attr_accessor :name
+    attr_accessor :name, :skill
 
-    def initialize(name)
-      raise FootballManager::PlayerError, 'You cannot create a player with no name' if name.nil?
-      @name = name
+    def initialize(name, skill)
+      @name = name if valid?(name, 'name')
+      @skill = skill.to_i if valid?(skill, 'skill')
+    end
+
+    def valid?(attribute, attribute_name)
+      attribute.nil? ? fail(FootballManager::PlayerError, "You cannot create a player with no #{attribute_name}") : true
     end
   end
 end
--- spec/football-manager/player_spec.rb
+++ spec/football-manager/player_spec.rb
@@ -1,18 +1,31 @@
 require 'spec_helper'
 
 describe FootballManager::Player do
-  subject { FootballManager::Player.new(name)}
+  subject { FootballManager::Player.new(name, skill)}
 
-  context 'valid with a name' do
+  context 'valid' do
     let(:name) { 'player 1' }
+    let(:skill) { '5' }
+
     its(:name) { should == 'player 1' }
+    its(:skill) { should == 5 }
   end
 
   context 'invalid with no name' do
     let(:name) { nil }
+    let(:skill) { '5' }
 
     it 'raises an error' do
       lambda { subject }.should raise_error(FootballManager::PlayerError, 'You cannot create a player with no name')
+    end
+  end
+
+  context 'invalid with no skill' do
+    let(:name) { 'player 1' }
+    let(:skill) { nil }
+
+    it 'raises an error' do
+      lambda { subject }.should raise_error(FootballManager::PlayerError, 'You cannot create a player with no skill')
     end
   end
 end
