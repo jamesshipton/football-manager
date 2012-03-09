4aefd696d8ca75f438ca6dc1e9767bc7f3437347
James Shipton <ionysis@gmail.com>
2012-03-09 00:36:56 +0000

    Add partition algorithm

--- lib/football-manager/team_picker.rb
+++ lib/football-manager/team_picker.rb
@@ -1,9 +1,19 @@
 module FootballManager
   module TeamPicker
     def self.calculate_fair_teams(players)
-      players.partition { |player| players.index(player) < players.size/2 }.collect do |partitioned_players|
-        FootballManager::Team.new(partitioned_players)
+      players_in_descending_order = players.sort { |a,b| b.skill <=> a.skill }
+      team_a, team_b  = FootballManager::Team.new, FootballManager::Team.new
+
+      players_in_descending_order.partition do |player|
+        if team_a.empty? || team_a.points <= team_b.points
+          team_a << player
+        else
+          team_b << player
+        end
       end
+
+      return [team_a, team_b]
     end
   end
 end
+
