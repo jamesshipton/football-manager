019a2a40bbaf44b8c74921d1f94b0624f8eb15cf
James Shipton <ionysis@gmail.com>
2012-03-14 07:45:07 +0000

    Refactor TeamPicker.calculate_fair_players

--- lib/football-manager/team_picker.rb
+++ lib/football-manager/team_picker.rb
@@ -1,17 +1,9 @@
 module FootballManager
   class TeamPicker
     def self.calculate_fair_teams(players)
-      # players_in_descending_order = players.sort { |a,b| b.skill <=> a.skill }
-      players_in_descending_order = []
-
-      5.downto(1) do |i|
-        players_in_descending_order <<
-          players.select { |player| player.skill == i }.sort_by {rand}
-      end
-
       team_a, team_b  = FootballManager::Team.new, FootballManager::Team.new
 
-      players_in_descending_order.flatten.partition do |player|
+      players_in_descending_order(players).each do |player|
         if team_a.empty? || team_a.points <= team_b.points
           team_a << player
         else
@@ -21,6 +13,13 @@ module FootballManager
 
       return [team_a, team_b]
     end
+
+    private
+      def self.players_in_descending_order(players)
+        5.downto(1).collect do |i|
+          players.select { |player| player.skill == i }.sort_by {rand}
+        end.flatten!
+      end
   end
 end
 
