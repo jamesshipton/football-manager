49f531ec8fe9e7bf7b0eaa81c534b91e10fe96da
James Shipton <ionysis@gmail.com>
2012-03-17 10:46:34 +0000

    format

--- features/support/choose_teams_support.rb
+++ features/support/choose_teams_support.rb
@@ -6,6 +6,6 @@ def check_both_teams_are_equal(number_of_players, points)
 end
 
 def check_team_has_the_following(number_of_players, points)
-    pattern = "Team (A|B) has #{number_of_players} player[s]? and #{points} points"
-    assert_matching_output(pattern, all_output)
+  pattern = "Team (A|B) has #{number_of_players} player[s]? and #{points} points"
+  assert_matching_output(pattern, all_output)
 end
